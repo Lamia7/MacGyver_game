5576e19fe2273f896ead0735b706151ce18cc6ef
lamia <elralimi.lamia@gmail.com>
2020-01-31 17:21:56 +0100

    🍱 Added images

---
+++ config.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+""" Loads the different data that the game needs """
+
+# Images
+HOME = "images/home.png"
+BACKGROUND = "images/background.jpg"
+WALLS = "images/walls.png"
+MACGYVER = "images/hero.png"
+GUARDIAN = "images/guardian.png"
+ITEM0 = "images/needle.png"
+ITEM1 = "images/ether.png"
+ITEM2 = "images/tube.png"
